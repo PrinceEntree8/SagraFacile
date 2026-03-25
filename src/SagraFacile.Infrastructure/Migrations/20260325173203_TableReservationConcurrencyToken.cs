394d10800296b66bc37759c78fad435aa63b5f6f
princeentree8 <edo.macri@gmail.com>
2026-03-25 18:34:08 +0100

    Added concurrency token to `TableReservations` for optimistic concurrency handling.

--- src/SagraFacile.Infrastructure/Migrations/20260325173203_TableReservationConcurrencyToken.cs
+++ src/SagraFacile.Infrastructure/Migrations/20260325173203_TableReservationConcurrencyToken.cs
@@ -1,11 +1,11 @@
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace SagraFacile.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class AddTableReservationConcurrencyToken : Migration
+    public partial class TableReservationConcurrencyToken : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
