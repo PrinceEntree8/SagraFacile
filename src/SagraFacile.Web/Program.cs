2797fa260160108409222d60dbd8030ecb0beae6
princeentree8 <edo.macri@gmail.com>
2026-06-27 04:02:04 +0200

    removed serving from webapp

--- src/SagraFacile.Web/Program.cs
+++ src/SagraFacile.Web/Program.cs
@@ -130,14 +130,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 app.MapHub<ReservationHub>("/hubs/reservations");
-app.MapFallbackToFile("index.html");
 
 app.Run();
